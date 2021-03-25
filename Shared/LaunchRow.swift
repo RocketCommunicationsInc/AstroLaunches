7acb5c183bd788b5c9f1239b5dfce96ff6b4bef2
Jeff Hokit <jhokit@rocketcom.com>
2021-03-25 14:26:53 -0700

    improved layout, build 9

--- Shared/LaunchRow.swift
+++ Shared/LaunchRow.swift
@@ -13,8 +13,34 @@ struct LaunchRow: View {
     
     var body: some View {
         VStack {
-            Image("launch").resizable().frame(minWidth: 10, idealWidth: 100, maxWidth: .infinity, minHeight: 10, idealHeight: 100, maxHeight: 100, alignment: .center)
-            Text(launch.name)
+            ZStack(alignment:.bottomTrailing){
+                Image("launch").resizable().frame(minWidth: 10, idealWidth: 100, maxWidth: .infinity, minHeight: 10, idealHeight: 100, maxHeight: 100, alignment: .center)
+                    .cornerRadius(5)
+                HStack {
+                    VStack{
+                        Text("02 08 24 45").foregroundColor(.white).bold()
+                        Text("D H M S").foregroundColor(.white)
+                    }
+                }
+            }
+            HStack {
+                Text(launch.name).font(.title2).bold()
+                Spacer()
+                Image(systemName: "bell.fill").foregroundColor(.astroUITint)
+                    .font(Font.system(.body))
+            }.padding(.vertical, 4)
+            HStack {
+                Image(systemName: "calendar")
+                    .font(.body).foregroundColor(.secondary)
+                Text("Date").font(.body).foregroundColor(.secondary)
+                Spacer()
+                Image(systemName: "clock").foregroundColor(.secondary)
+                Text("Time").font(.body).foregroundColor(.secondary)
+                Spacer()
+                Image(systemName: "cloud.sun").foregroundColor(.secondary)
+
+                Text("Weather").font(.body).foregroundColor(.secondary)
+            }.padding(.vertical, 4)
 
         }
     }
