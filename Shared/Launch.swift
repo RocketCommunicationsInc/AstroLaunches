f26b3711d5bf751370e1fe94df3047883553dcc2
Jeff Hokit <jhokit@rocketcom.com>
2021-07-16 15:53:09 -0700

    Document more database fields

--- Shared/Launch.swift
+++ Shared/Launch.swift
@@ -22,18 +22,20 @@ struct Launch{
     let windowOpenDate:Date?// the date and time the launch window opens, if known
     let windowCloseDate:Date?// the date and time the launch window closes, if known
     let temperature:Float? // the forecast temperature at launch, in farenheit, if known
-    let weather:Weather? //
+    let weather:Weather? // an enum of basic weather conditions
    // let win_open:String?
     
 
 
-    
+    // Parse a LaunchReply, see which fields were returned and convert to Swift types
     init(_ launchReply:LaunchReply)
     {
         name = launchReply.name
         date  = launchReply.date_str
         temperature  = launchReply.weather_temp
         
+        // weather_icon is one of these icon values, https://erikflowers.github.io/weather-icons/
+        // Simplify and map down to our Weather enum
         if let weatherIcon = launchReply.weather_icon
         {
             if weatherIcon.contains("cloud")
@@ -58,16 +60,19 @@ struct Launch{
             weather = nil
         }
 
+        // Convert dates using our ZuluDateFormatter, which can handle some peciliaries with this format
+
+        // t0 = The date and time of the planned launch time (T-0) in ISO 8601 format
         if let t0date = launchReply.t0
         {
             preciseDate = ZuluDateFormatter.sharedInstance.date(from: t0date)
-
         }
         else
         {
             preciseDate = nil
         }
         
+        // win_Open = The date and time of the opening of the launch window in ISO 8601 format
         if let windowOpen = launchReply.win_open
         {
             let thewindowOpenDate = ZuluDateFormatter.sharedInstance.date(from: windowOpen)
@@ -78,6 +83,7 @@ struct Launch{
             windowOpenDate = nil
         }
         
+        // win_Close = The date and time of the closing of the launch window in ISO 8601 format
         if let windowClose = launchReply.win_close
         {
             windowCloseDate = ZuluDateFormatter.sharedInstance.date(from: windowClose)
@@ -86,7 +92,6 @@ struct Launch{
         {
             windowCloseDate = nil
         }
-        
         
     }
 }
